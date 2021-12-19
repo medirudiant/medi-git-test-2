0fc4d8a9c2489e545cc3a735f8efc7423a9bb529
medi rudiantoni <medirudiantoni2@gmail.com>
2021-12-19 14:10:37 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>medi git tet 2</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed necessitatibus ex odio rem ducimus minus error, dolorem vel, accusantium consequuntur debitis repellat magni amet explicabo saepe tempore nemo veritatis ipsa fuga. Amet odit optio nihil temporibus doloribus molestiae hic mollitia rerum officia, voluptatem voluptates nostrum quod incidunt dolores corporis, accusantium assumenda esse inventore, cumque nemo est. Totam accusantium dolorem fugiat ratione ipsum veniam eos non exercitationem porro magni, odio, quam nobis ab, minima eius sed ut quae molestias sequi quo. Hic, odio ullam nisi temporibus adipisci atque maxime dolorem, nihil aperiam nam sit fuga quidem voluptatibus eveniet veritatis quibusdam magni ratione voluptate? Necessitatibus, odio ad! Officiis reiciendis impedit qui nostrum atque assumenda sunt illum soluta eveniet, reprehenderit, sapiente voluptatibus quo.</p>
+    <ul>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, expedita?</li>
+    </ul>
 </body>
 </html>
